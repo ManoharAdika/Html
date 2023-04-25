c203f158ffb13d524fc8501a864dba80e3fa4d33
adikamanoharreddy <adikamanoharreddy123@gmail.com>
2023-04-25 10:21:10 +0530

    initial commit

---
+++ ShowData.js
@@ -0,0 +1,14 @@
+function ShowData(){
+
+   // alert('Good person never change the attitude')
+  //   document.getElementById('btn1').style.backgroundColor = 'Red'; 
+    document.write("Manohar")
+}
+
+function data1(){
+    
+    document.getElementById('abc').style.backgroundColor = 'blue'; 
+    alert('Good person never change the attitude')
+  
+  //  document.write("Manohar")
+}
