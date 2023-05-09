5bfc2ef5dcd5bde3705fa6253c81184e9f0c0a40
adikamanoharreddy <adikamanoharreddy123@gmail.com>
2023-05-09 09:50:23 +0530

    added js file

---
+++ data.js
@@ -0,0 +1,12 @@
+function onmouseover1(){
+    let mover= document.getElementById('login');
+    mover.style.backgroundColor = 'red';
+    mover.style.backgroundColor = 'green';
+    alert(data);
+}
+function onmouseout1(){
+    let mover= document.getElementById('login');
+    mover.style.backgroundColor = '#fff';
+    
+    alert(data);
+}
